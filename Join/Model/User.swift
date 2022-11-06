822f4985688fd60a62c4fdfd568c74ee7e51c6e2
Riley <dakimi07@gmail.com>
2022-11-06 23:47:57 +0800

    chore: modify mock login data

--- Join/Model/User.swift
+++ Join/Model/User.swift
@@ -15,8 +15,8 @@ let riley = User(
     thumbnailURL: "https://firebasestorage.googleapis.com:443/v0/b/join-82f54.appspot.com/o/DA32761A-2775-414C-95E8-F01DCB2CDD66?alt=media&token=c6ac5b7e-1e53-4d0b-813a-eb12c051bf6d"
 )
 
-let yichen = User(
-    id: "6z63wggZ1FdOnBEA7Q6s", name: "Yi Chen",
+let someOne = User(
+    id: "6z63wggZ1FdOnBEA7Q6s", name: "Some One",
     email: "ccc@gmail.com",
     thumbnailURL: "https://firebasestorage.googleapis.com:443/v0/b/join-82f54.appspot.com/o/25E3357B-A23A-4852-884A-B424FB6ED3FC?alt=media&token=75cc10c7-534b-4e7f-a9fd-45ddcd73d76e"
 )
